4197e068ab4b50f0c9300a469a845694773bcc33
DESKTOP-FQBJS4T\Hacky_Shaggy <karthick.psgtec12@gmail.com>
2017-04-03 07:58:14 +0530

    Updated Forms

--- src/app/components/create-profile/create-profile.component.ts
+++ src/app/components/create-profile/create-profile.component.ts
@@ -1,5 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+
+import 'rxjs/add/operator/debounceTime';
+
+function passwordMatcher(c: AbstractControl): {[key: string]: boolean} | null {
+    let passwordControl = c.get('profileFPassword');
+    let confirmControl = c.get('profileCPassword');
+
+    if (passwordControl.pristine || confirmControl.pristine) {
+      return null;
+    }
+
+    if (passwordControl.value === confirmControl.value) {
+        return null;
+    }
+    return { 'match': true };
+ }
 
 @Component({
   selector: 'app-create-profile',
@@ -10,26 +26,48 @@ export class CreateProfileComponent implements OnInit {
   createForm: FormGroup;
   // loginPageDeclaration: LoginPageDeclaration = new LoginPageDeclaration();
 
+  passwordMessage: string;
+
+  private validationMessages = {
+    required: " password required",
+    pattern: "password pattern wrong"
+  }
+
   constructor(private formBuilderProperty: FormBuilder) { }
 
   ngOnInit() {
     this.createForm = this.formBuilderProperty.group({
-            profileUName: ['', [Validators.required, Validators.minLength(3)]],
-            profileFPassword: ['', [Validators.required, Validators.maxLength(50)]],
-            profileCPassword: ['', [Validators.required, Validators.minLength(3)]],
-            profileAge: ['', [Validators.required, Validators.maxLength(50)]],
-            profileSex: ['', [Validators.required, Validators.maxLength(50)]],
-            profileOccupation: ['', [Validators.required, Validators.minLength(3)]],
-            profileMobile: ['', [Validators.required, Validators.maxLength(50)]],
-            profileEmail: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+')]]
-        });
+      profileUName: ['', [Validators.required, Validators.minLength(3)]],
+      passwordGroup: this.formBuilderProperty.group({
+        profileFPassword: ['', [Validators.required, Validators.minLength(8)]],
+        profileCPassword: ['', [Validators.required, Validators.minLength(8)]],
+      },{validator:passwordMatcher}),
+      profileAge: ['', [Validators.required]],
+      profileSex: ['', [Validators.required]],
+      profileOccupation: ['', [Validators.required, Validators.minLength(3)]],
+      profileMobile: ['', [Validators.required, Validators.pattern('')]],
+      profileEmail: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+')]]
+    });
 
     // console.log(this.createForm.get('profileUName').touched );
     // console.log(this.createForm.get('profileUName').dirty);
     // console.log(this.createForm.get('profileUName').valid);
+    const passwordCheck = this.createForm.get('passwordGroup.profileFPassword');
+    passwordCheck.valueChanges.debounceTime(1000).subscribe(value =>
+      this.setMessage(passwordCheck));
+
   }
   save(): any {
-      alert('saved');
+    alert('saved');
   }
+
+  setMessage(c: AbstractControl): void {
+    this.passwordMessage = '';
+    if ((c.touched || c.dirty) && c.errors) {
+      this.passwordMessage = Object.keys(c.errors).map(key =>
+        this.validationMessages[key]).join(' ');
+    }
+  }
+
 
 }
